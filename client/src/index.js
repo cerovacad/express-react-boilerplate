8a0a3a63ea8a4b312ec06a312b03594e4c10daf7
cerovacad <cerovacad@gmail.com>
2018-06-18 19:30:02 +0200

    init

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends Component {
+    state = { 
+        hello: ''
+     }
+
+     componentDidMount = () => {
+        axios.get('/api/')
+        .then((response) => {
+          console.log(response.data)
+          this.setState({ hello: response.data.hello })
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+     }
+
+    render() {
+        return (
+            <div>
+                hello {this.state.hello}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- server/app.js
+++ server/app.js
@@ -8,13 +8,15 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
